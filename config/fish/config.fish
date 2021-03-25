03010de1888d46eff5294fc4a426be5165d7ebc7
Juan Knebel <juanknebel@gmail.com>
2021-03-25 20:17:23 -0300

    Remove bash files, update fish files

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,2 @@
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /home/$USER/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-
 set -x SPOTIPY_CLIENT_ID XXXX
 set -x SPOTIPY_CLIENT_SECRET XXXX
