7f67c789e2200e7ad0be0febaf7abd65e9f94594
Juan <juanknebel@gmail.com>
2021-06-01 11:01:35 -0300

    Update config-osx.fish

--- config/fish/config-osx.fish
+++ config/fish/config-osx.fish
@@ -5,3 +5,6 @@ set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
 # init
 status is-login; and pyenv init --path | source
 pyenv init - | source
+
+### Paths
+set -U fish_user_paths /usr/local/sbin $fish_user_paths
