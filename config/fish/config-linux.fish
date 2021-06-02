fd2ae7fd9afeece765cf1e7c3636c5efba670ef6
Juan <juanknebel@gmail.com>
2021-06-02 12:10:02 -0300

    Update config-linux.fish

--- config/fish/config-linux.fish
+++ config/fish/config-linux.fish
@@ -4,5 +4,10 @@ if command -v pyenv 1>/dev/null 2>&1
   pyenv init - | source
 end
 
+### Alias
+# status with konsole
+alias stats-glances='konsole --profile stats -e glances'
+alias stats-gpu='konsole --profile stats -e gpustat -cp --watch'
+
 ### Snap
 #set PATH /var/lib/snapd/snap/bin $PATH
