bb866ae63d03b7a0106960488cee7da513c7d24f
Juan Knebel <juanknebel@gmail.com>
2021-11-02 11:39:00 -0300

    Remove conda configuration

--- config/fish/config-osx.fish
+++ config/fish/config-osx.fish
@@ -2,8 +2,3 @@
 set -U fish_user_paths /usr/local/sbin $fish_user_paths
 set -U fish_user_paths /usr/local/opt/mysql-client/bin $fish_user_paths
 set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
