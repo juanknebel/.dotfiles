7fae9c5059d719b26acc2a866e0389c0a8dbc603
Juan Knebel <juanknebel@gmail.com>
2022-12-20 16:15:59 -0300

    Modify the script to run with fish shell.

--- dev_config_init.sh
+++ dev_config_init.sh
@@ -1,89 +1,138 @@
-# Essentials
-sudo apt install -y git build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget llvm libncurses-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl;
+#!/usr/bin/fish
 
-# Dotfiles
-git clone git@github.com:juanknebel/.dotfiles.git;
+function setup_basic_install
+    echo "**** SOFTWARE ****"
+    echo "Installing essentials"
+    # Ubuntu Essentials
+    # sudo apt install -y git build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget llvm libncurses-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl xclip;
+    echo "Done."
+end
 
-ln -s $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf;
+function setup_tmux
+    echo "*** TMUX ****"
+    ln -s $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf;
+    echo "Done."
+end
 
-# Git
-ln -s $HOME/.dotfiles/gitignore_global $HOME/.gitignore_global;
-git config --global core.excludesfile $HOME/.gitignore_global;
-git config --global init.defaultBranch main;
-git config --global user.email "juanknebel@gmail.com";
-git config --global user.name "Juan Knebel";
+function setup_git
+    # SSH
+    # ssh-keygen -t ed25519 -b 4096 -C "juanknebel@gmail.com";
+    # eval (ssh-agent -c);
+    # ssh-add $HOME/.ssh/id_ed25519;
+    # xclip -selection clipboard < $HOME/.ssh/id_ed25519.pub;
+    # pbcopy < $HOME/.ssh/id_ed25519.pub; #for macos
+    echo "**** GIT ****"
+    ln -s $HOME/.dotfiles/gitignore_global $HOME/.gitignore_global;
+    git config --global core.excludesfile $HOME/.gitignore_global;
+    git config --global init.defaultBranch main;
+    git config --global user.email "juanknebel@gmail.com";
+    git config --global user.name "Juan Knebel";
+    echo "Done."
+end
 
-# Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh;
-mkdir -p $HOME/.config/rustfmt;
-ln -s $HOME/.dotfiles/config/rustfmt/rustfmt.toml $HOME/.config/rustfmt;
+function setup_rust
+    echo "**** RUST ****"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh;
+    mkdir -p $HOME/.config/rustfmt;
+    ln -s $HOME/.dotfiles/config/rustfmt/rustfmt.toml $HOME/.config/rustfmt;
+    echo "Done."
+end
 
-# Pyenv
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv;
-git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv;
-pyenv install 3.11;
-pyenv global system;
-pyenv local 3.11;
+function setup_python
+    echo "**** PYTHON ****"
+    echo "Pyenv"
+    # Pyenv
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv;
+    git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv;
+    pyenv install 3.11;
+    pyenv global system;
+    pyenv local 3.11;
 
-# Poetry
-curl -sSL https://install.python-poetry.org | python3 -;
-poetry completions fish > ~/.config/fish/completions/poetry.fish;
+    echo "Poetry"
+    # Poetry
+    curl -sSL https://install.python-poetry.org | python3 -;
+    poetry completions fish > ~/.config/fish/completions/poetry.fish;
+    echo "Done."
+end
 
-# Java
-curl -s "https://get.sdkman.io" | bash ;
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher;
-fisher install reitzig/sdkman-for-fish@v1.4.0;
+function setup_java
+    echo "**** JAVA ****"
+    echo "SDK Manager ..."
+    curl -s "https://get.sdkman.io" | bash ;
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher;
+    fisher install reitzig/sdkman-for-fish@v1.4.0;
+    echo "Done."
+end
 
-# SSH
-#ssh-keygen -t ed25519 -b 4096 -C "juanknebel@gmail.com";
-eval (ssh-agent -c);
-ssh-add $HOME/.ssh/id_ed25519;
-#xclip -selection clipboard < $HOME/.ssh/id_ed25519.pub;
-#pbcopy < $HOME/.ssh/id_ed25519.pub; #for macos
+function setup_fish_shell
+    echo "*** FISH ****"
+    echo "Shell"
+    # Fish shell
+    mkdir -p $HOME/.config/fish/conf.d;
+    mkdir -p $HOME/.config/fish/functions;
 
+    echo "Extras"
+    # Fish extras
+    if test -e "$HOME/.config/fish/config.fish"
+        rm $HOME/.config/fish/config.fish;
+    end
+    ln -s $HOME/.dotfiles/config/fish/config.fish $HOME/.config/fish/config.fish;
+    ln -s $HOME/.dotfiles/config/fish/config-alias-abbr.fish $HOME/.config/fish/config-alias-abbr.fish;
+    ln -s $HOME/.dotfiles/config/fish/functions/fish_greeting.fish $HOME/.config/fish/functions/fish_greeting.fish;
 
-# Fish shell
-#sudo usermod --shell /bin/fish $USER
-mkdir -p $HOME/.config/fish/conf.d;
-mkdir -p $HOME/.config/fish/functions;
+    # Coding
+    ln -s $HOME/.dotfiles/config/fish/config-dev.fish $HOME/.config/fish/config-dev.fish;
 
-# Fish extras
-rm $HOME/.config/fish/config.fish;
-ln -s $HOME/.dotfiles/config/fish/config.fish $HOME/.config/fish/config.fish;
-ln -s $HOME/.dotfiles/config/fish/config-alias-abbr.fish $HOME/.config/fish/config-alias-abbr.fish;
-ln -s $HOME/.dotfiles/config/fish/functions/fish_greeting.fish $HOME/.config/fish/functions/fish_greeting.fish;
+    # Linux
+    ln -s $HOME/.dotfiles/config/fish/config-linux.fish $HOME/.config/fish/config-linux.fish;
+    # For some local configuration that is only for this machine
+    touch $HOME/.config/fish/config-local.fish;
 
-# Coding
-ln -s $HOME/.dotfiles/config/fish/config-dev.fish $HOME/.config/fish/config-dev.fish;
+    echo "Fuzzy search"
+    # z fuzzy search
+    mkdir -p $HOME/.local/share/z;
+    touch $HOME/.local/share/z/data;
 
-# Linux
-ln -s $HOME/.dotfiles/config/fish/config-linux.fish $HOME/.config/fish/config-linux.fish;
-# For some local configuration that is only for this machine
-touch $HOME/.config/fish/config-local.fish;
+    cd $HOME/git;
+    git clone https://github.com/jethrokuan/z.git;
+    ln -s $HOME/git/z/functions/__z_add.fish $HOME/.config/fish/functions/__z_add.fish;
+    ln -s $HOME/git/z/functions/__z_clean.fish $HOME/.config/fish/functions/__z_clean.fish;
+    ln -s $HOME/git/z/functions/__z_complete.fish $HOME/.config/fish/functions/__z_complete.fish;
+    ln -s $HOME/git/z/functions/__z.fish $HOME/.config/fish/functions/__z.fish;
+    ln -s $HOME/git/z/conf.d/z.fish $HOME/.config/fish/conf.d/;
 
-# z fuzzy search
-mkdir -p $HOME/.local/share/z;
-touch $HOME/.local/share/z/data;
+    #### Command timer not install if starship is install
+    # cd $HOME/git
+    # git clone https://github.com/jichu4n/fish-command-timer.git;
+    # ln -s $HOME/git/fish-command-timer/conf.d/fish_command_timer.fish $HOME/.config/fish/conf.d;
+    source $HOME/.config/fish/config.fish;
+    echo "Done."
+end
 
-cd $HOME/git;
-git clone https://github.com/jethrokuan/z.git;
-ln -s $HOME/git/z/functions/__z_add.fish $HOME/.config/fish/functions/__z_add.fish;
-ln -s $HOME/git/z/functions/__z_clean.fish $HOME/.config/fish/functions/__z_clean.fish;
-ln -s $HOME/git/z/functions/__z_complete.fish $HOME/.config/fish/functions/__z_complete.fish;
-ln -s $HOME/git/z/functions/__z.fish $HOME/.config/fish/functions/__z.fish;
-ln -s $HOME/git/z/conf.d/z.fish $HOME/.config/fish/conf.d/;
+function setup_starfish_prompt
+    echo "**** STARSHIP PROMPT ****"
+    mkdir -p $HOME/.fonts;
+    cd $HOME;
+    curl -fLo firaCode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip;
+    unzip firaCode.zip -d $HOME/.fonts;
+    rm firaCode.zip;
+    echo 'starship init fish | source'>> $HOME/.config/fish/config-local.fish;
+    ln -s $HOME/.dotfiles/config/starship/starship.toml $HOME/.config/starship.toml;
+    echo "Done."
+end
 
-#### Command timer not install if starship is install
-#cd $HOME/git
-#git clone https://github.com/jichu4n/fish-command-timer.git;
-#ln -s $HOME/git/fish-command-timer/conf.d/fish_command_timer.fish $HOME/.config/fish/conf.d;
+function init
+    echo "**** INIT ****"
+    # git clone git@github.com:juanknebel/.dotfiles.git;
+    mkdir -p $HOME/git;
+    echo "Done."
+end
 
-# Starship prompt
-mkdir -p $HOME/.fonts;
-cd $HOME;
-curl -fLo firaCode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip;
-unzip firaCode.zip -d $HOME/.fonts;
-rm firaCode.zip;
-echo 'starship init fish | source'>> $HOME/.config/fish/config-local.fish;
-ln -s $HOME/.dotfiles/config/starship/starship.toml $HOME/.config/starship.toml;
-
+init
+setup_fish_shell
+setup_git
+setup_tmux
+setup_starfish_prompt
+setup_rust
+setup_python
+setup_java
