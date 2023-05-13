954079a0553401952deed595d712bc021fcf6eb3
Juan Knebel <juanknebel@gmail.com>
2023-05-12 23:21:15 -0300

    Add more plugins

--- config/nvim/lua/custom/plugins.lua
+++ config/nvim/lua/custom/plugins.lua
@@ -1,5 +1,9 @@
 local plugins = {
   {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
@@ -34,20 +38,46 @@ local plugins = {
   },
   {
     "mfussenegger/nvim-dap",
+    init = function()
+      require("core.utils").load_mappings("dap")
+    end
   },
   {
-    "saecki/crates.nvim",
+    'saecki/crates.nvim',
     ft = {"rust", "toml"},
     config = function(_, opts)
-      local crates = require('crates')
+      local crates  = require('crates')
       crates.setup(opts)
+      require('cmp').setup.buffer({
+        sources = { { name = "crates" }}
+      })
       crates.show()
+      require("core.utils").load_mappings("crates")
     end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    lazy = false,
+    config = function(_, opts)
+      require("nvim-dap-virtual-text").setup()
+    end
   },
   {
     "hrsh7th/nvim-cmp",
     opts = function()
       local M = require "plugins.configs.cmp"
+      -- M.completion.completeopt = "menu,menuone,noselect"
+      -- M.mapping["<CR>"] = cmp.mapping.confirm {
+      --   behavior = cmp.ConfirmBehavior.Insert,
+      --   select = false,
+      -- }
       table.insert(M.sources, {name = "crates"})
       return M
     end,
