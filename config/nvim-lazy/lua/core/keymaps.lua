8eab45136c12f8fdeabf5b2cd310c32b6275f16e
Juan Knebel <juanknebel@gmail.com>
2023-07-16 23:21:13 -0300

    More keymaps

--- config/nvim-lazy/lua/core/keymaps.lua
+++ config/nvim-lazy/lua/core/keymaps.lua
@@ -1,14 +1,17 @@
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 
 -- window management
 vim.keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
 vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+vim.keymap.set("n", "<leader>ml", "<C-w>>") -- resize to right
+vim.keymap.set("n", "<leader>mh", "<C-w><") -- resize to left
+vim.keymap.set("n", "<leader>mk", "<C-w>+") -- resize up
+vim.keymap.set("n", "<leader>mj", "<C-w>-") -- resize down
 vim.keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
 vim.keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
-
