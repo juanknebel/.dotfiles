b62cb11d0097c65cbcce8180ae6a27ded1046625
Juan Knebel <juanknebel@gmail.com>
2023-12-29 01:39:45 -0300

    Third step for my new neovim configuration.

---
+++ config/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,43 @@
+return {
+
+	"goolord/alpha-nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+
+	config = function()
+		local alpha = require("alpha")
+		--local dashboard = require("alpha.themes.dashboard")
+		local startify = require("alpha.themes.startify")
+
+		startify.section.header.val = {
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                     ]],
+			[[       ████ ██████           █████      ██                     ]],
+			[[      ███████████             █████                             ]],
+			[[      █████████ ███████████████████ ███   ███████████   ]],
+			[[     █████████  ███    █████████████ █████ ██████████████   ]],
+			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+		}
+
+		_Gopts = {
+			position = "center",
+			hl = "Type",
+			-- wrap = "overflow";
+		}
+
+		startify.opts.opts.noautocmd = true
+		alpha.setup(startify.opts)
+	end,
+}
