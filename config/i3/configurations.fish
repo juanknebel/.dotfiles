dc26124e519642a8dbb3e371b05878f515f92778
Juan Knebel <juanknebel@gmail.com>
2024-02-25 02:06:02 -0300

    i3 now lock the screen when close the lid.

--- config/i3/configurations.fish
+++ config/i3/configurations.fish
@@ -1,5 +1,7 @@
 #/usr/bin/fish
 
 sudo ln -s $HOME/.dotfiles/config/i3/resources/90-touchpad.conf /etc/X11/xorg.conf.d/90-touchpad.conf;
+sudo ln -s $HOME/.dotfiles/config/i3/resources/lock@.service /usr/lib/systemd/system/lock@.service;
+sudo systemctl enable lock@$USER.service;
 ln -s $HOME/.dotfiles/config/i3/i3blocks.conf $HOME/.config/i3/i3blocks.conf;
 ln -s $HOME/.dotfiles/config/i3/config $HOME/.config/i3/config;
