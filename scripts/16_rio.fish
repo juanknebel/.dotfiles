cf35ace073954f6ebf5f38fd7a1d036caf8b47eb
Juan Knebel <juanknebel@gmail.com>
2025-05-25 22:58:11 -0300

    Add more apps. Rio confg. Leftwm.

--- scripts/16_rio.fish
+++ scripts/16_rio.fish
@@ -1,6 +1,7 @@
 #!/usr/bin/fish
 
 function config_rio
+    # themes: https://github.com/mbadolato/iTerm2-Color-Schemes/tree/master
     echo "**** Rio ****"
     if test -f $HOME/.config/rio/config.toml
         mv $HOME/.config/rio/config.toml $HOME/.config/rio/config.toml.bak;
